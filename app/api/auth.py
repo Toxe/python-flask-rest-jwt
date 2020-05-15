242d3f0c3167b801448d921f57fcf4f1b6fafc37
Toxe <toxe@toxe.net>
2020-05-15 09:24:30 +0200

    Add JWT refresh tokens

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,5 +1,9 @@
 from flask import jsonify, request
-from flask_jwt_extended import create_access_token, decode_token
+from flask_jwt_extended import (
+    create_access_token, create_refresh_token,
+    jwt_refresh_token_required, get_jwt_identity,
+    decode_token
+)
 from app import db
 from app.api import bp
 from app.errors import error_response
@@ -22,5 +26,16 @@ def login():
     if not user or user.password != password:
         return error_response(401, "Username or password invalid.")
 
+    access_token = create_access_token(identity=user.id)
+    refresh_token = create_refresh_token(identity=user.id)
+    return jsonify(access_token=access_token, refresh_token=refresh_token)
+
+
+@bp.route("/refresh", methods=["POST"])
+@jwt_refresh_token_required
+def refresh():
+    user = db.get_user(get_jwt_identity())
+    if not user:
+        return error_response(401, "Unknown user.")
     access_token = create_access_token(identity=user.id)
     return jsonify(access_token=access_token)
--- tests/test_api_auth.py
+++ tests/test_api_auth.py
@@ -6,9 +6,13 @@ def test_login(client):
     assert r.status_code == 200
     assert r.is_json
     access_token = r.get_json().get("access_token")
+    refresh_token = r.get_json().get("refresh_token")
     assert access_token is not None
+    assert refresh_token is not None
     token_data = decode_token(access_token)
+    refresh_token_data = decode_token(refresh_token)
     assert token_data.get("identity") == 1
+    assert refresh_token_data.get("identity") == 1
 
 
 def test_login_username_missing(client):
@@ -45,3 +49,44 @@ def test_login_with_wrong_password(client):
     data = r.get_json()
     assert data.get("message") == "Username or password invalid."
     assert "access_token" not in data
+
+
+def test_refresh_token(client):
+    r = client.post("/api/login", json={"username": "user", "password": "password"})
+    refresh_token = r.get_json().get("refresh_token")
+    old_access_token = r.get_json().get("access_token")
+    r = client.post("/api/refresh", headers={"Authorization": "Bearer {}".format(refresh_token)})
+    assert r.status_code == 200
+    assert r.is_json
+    access_token = r.get_json().get("access_token")
+    assert access_token is not None
+    assert access_token != old_access_token
+    token_data = decode_token(access_token)
+    assert token_data.get("identity") == 1
+
+
+def test_cannot_call_refresh_for_unknown_user(client):
+    r = client.post("/api/login", json={"username": "user", "password": "password"})
+    access_token = r.get_json().get("access_token")
+    refresh_token = r.get_json().get("refresh_token")
+    user_id = decode_token(access_token).get("identity")
+    assert client.delete("/api/users/{}".format(user_id), headers={"Authorization": "Bearer {}".format(access_token)}).status_code == 204
+    assert client.post("/api/refresh", headers={"Authorization": "Bearer {}".format(refresh_token)}).status_code == 401
+
+
+def test_cannot_call_refresh_with_access_token(client):
+    r = client.post("/api/login", json={"username": "user", "password": "password"})
+    access_token = r.get_json().get("access_token")
+    r = client.post("/api/refresh", headers={"Authorization": "Bearer {}".format(access_token)})
+    assert r.status_code == 422
+    assert r.is_json
+    assert r.get_json().get("error") == "Only refresh tokens are allowed"
+
+
+def test_cannot_call_protected_api_with_refresh_token(client):
+    r = client.post("/api/login", json={"username": "user", "password": "password"})
+    refresh_token = r.get_json().get("refresh_token")
+    r = client.delete("/api/ships/1", headers={"Authorization": "Bearer {}".format(refresh_token)})
+    assert r.status_code == 422
+    assert r.is_json
+    assert r.get_json().get("error") == "Only access tokens are allowed"
